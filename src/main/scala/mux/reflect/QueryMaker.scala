c27cda0fdaa556c459f12f4a686187b1859da40a
= <=>
2019-05-06 15:46:40 +0530

    Code clean-up #2

--- src/main/scala/mux/reflect/QueryMaker.scala
+++
@@ -1,7 +0,0 @@
-
-package mux.reflect
-
-trait QueryMaker {
-  def makeQuery(pid:String, reqName:String, reqData:String):String
-  def isConnected:Boolean
-}
--- src/main/scala/mux/reflect/QueryResponder.scala
+++
@@ -1,7 +0,0 @@
-
-package mux.reflect
-
-trait QueryResponder {
-  def getResp(pid:String, reqName:String, reqDataJson:String, useJavaSerialization:Boolean) = Proxy.getResponse(pid, reqName, reqDataJson, useJavaSerialization)  
-  def getRespJavaObject(pid:String, reqName:String, reqDataJson:String) = Proxy.getResponseJavaObject(pid, reqName, reqDataJson)  
-}
